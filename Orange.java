8768fedde08a1b74b06d120086c595cbe03f2d09
AnnKorosteliova <66014607+AnnKorosteliova@users.noreply.github.com>
2020-07-21 10:19:01 +0300

    Add files via upload

---
+++ Orange.java
@@ -0,0 +1,23 @@
+package fruits;
+
+public class Orange {
+
+	private int weight; //gramms
+	public static int conversionCoefficient = 60; //fruit to juice conversion factor
+
+	public Orange() {}
+
+	public Orange(int weight) {
+		setWeight(weight);
+	}
+		
+	public int getWeight() {
+		return weight;
+	}
+
+	public void setWeight(int weight) {
+		if(weight >= 10 && weight <= 1000)		
+			this.weight = weight;
+	}
+	
+}
