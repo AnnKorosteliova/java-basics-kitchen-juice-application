8111fc5cb10b821f27f85b52f497cf35d6b9ef13
AnnKorosteliova <66014607+AnnKorosteliova@users.noreply.github.com>
2020-07-21 09:29:22 +0300

    Add files via upload

---
+++ Blender.java
@@ -0,0 +1,31 @@
+package kitchen;
+
+import fruits.Orange;
+import liquids.Juice;
+
+public class Blender {
+
+	private String brand;
+
+	public Blender() {}
+	
+	public Blender(String brand) {  
+		setBrand(brand);
+	}
+	
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		if( brand.trim().equals("") || brand.trim().equals(null))
+			System.out.println("Uncorrect brand name");
+	    else
+	    	this.brand = brand;
+	}
+	
+	public Juice makeJuice(Orange orange) {
+		Juice juice = new Juice(orange.getWeight());
+		return juice;
+	}
+}
